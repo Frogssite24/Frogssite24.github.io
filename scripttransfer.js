edc61fef896c5f6ccdd85a4e2d4bed36436b308e
frogssite24 <heitormiguelribeiro150611@gmail.com>
2024-11-27 18:44:23 -0300

    Update scripttransfer.js
    jihjnjijoklh

--- scripttransfer.js
+++ scripttransfer.js
@@ -1,1 +1,14 @@
+// Obtendo os elementos do formulário
+var nome = document.getElementById('nome');
+var valor = document.getElementById('valor');
+var senha = document.getElementById('senha');
+var botaosubmit = document.getElementById('botaosubmit');
+
+  // Validação simples
+  if (!valor.value || !senha.value) {
+    alert('Por favor, preencha todos os campos!');
+    return;
+  }
+
+
 
